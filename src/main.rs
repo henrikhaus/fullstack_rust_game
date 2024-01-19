4bc6bf93613c041271cf0d2f6940274e291faa30
Anders <anders.morille@gmail.com>
2024-01-19 14:08:03 +0100

    Functioning server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::fs;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -6,6 +8,7 @@ async fn main() -> std::io::Result<()> {
     App::new()
         .service(hello)
         .service(echo)
+        .service(get_user_by_id)
   })
       .bind(("127.0.0.1", 8080))?
       .run()
@@ -17,8 +20,35 @@ async fn hello() -> impl Responder {
   HttpResponse::Ok().body("Hello world!")
 }
 
+#[get("/user/{user_id}")]
+async fn get_user_by_id(path: web::Path<String>) -> impl Responder {
+  let user_id = path.into_inner();
+  let users = read_db();
+  dbg!(&users);
+  let user = users.iter().find(|u| {
+    u.name == user_id
+  });
+
+  return match user {
+    None => { HttpResponse::NotFound().body("The user was not found") }
+    Some(u) => { HttpResponse::Ok().json(u) }
+  };
+}
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
   HttpResponse::Ok().body(req_body)
 }
 
+
+pub fn read_db() -> Vec<User> {
+  let contents = fs::read_to_string("users.json")
+      .expect("Should have been able to read the file");
+  serde_json::from_str(&contents).expect("Should be valid JSON")
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct User {
+  name: String,
+  age: u16,
+}
