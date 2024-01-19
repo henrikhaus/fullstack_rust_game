5227b9432c51abb092a08e8a3586199834e257b9
Anders <anders.morille@gmail.com>
2024-01-19 13:52:17 +0100

    Added main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+        .service(hello)
+        .service(echo)
+  })
+      .bind(("127.0.0.1", 8080))?
+      .run()
+      .await
 }
 
 #[get("/")]
@@ -14,6 +22,3 @@ async fn echo(req_body: String) -> impl Responder {
   HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-  HttpResponse::Ok().body("Hey there!")
-}
