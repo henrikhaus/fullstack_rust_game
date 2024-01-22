3a7416e7400154ee6de35b2386844d850795a4aa
Anders <anders.morille@gmail.com>
2024-01-22 23:01:50 +0100

    Nice update

--- backend/src/route/user.rs
+++ backend/src/route/user.rs
@@ -8,6 +8,8 @@ use serde::Deserialize;
 use sqlx::{Error, PgPool};
 use std::str::FromStr;
 use uuid::Uuid;
+use crate::service::db::postgres::PgRepo;
+use crate::service::db::postgres::poker_game::{PokerGame, PokerGamePgRepo};
 
 pub struct UserController;
 
@@ -26,80 +28,86 @@ const PATH_ID: &str = "/user/{user_id}";
 /// - PUT /user/{user_id}
 /// - DELETE /user/{user_id}
 pub fn user_scope() -> Scope {
-    web::scope("/user")
-        .service(
-            web::resource("")
-                .name("users")
-                .get(list_users)
-                .post(create_user),
-        )
-        .service(
-            web::resource("/{user_id}")
-                .name("user_detail")
-                .get(get_user)
-                .put(update_user)
-                .delete(delete_user),
-        )
+  web::scope("/user")
+      .service(
+        web::resource("")
+            .name("users")
+            .get(list_users)
+            .post(create_user),
+      )
+      .service(
+        web::resource("/{user_id}")
+            .name("user_detail")
+            .get(get_user)
+            .put(update_user)
+            .delete(delete_user),
+      )
 }
 
 async fn list_users(pool: web::Data<PgPool>) -> impl Responder {
-    let user_repo = UserPgRepo::new(pool.get_ref());
+  let user_repo = UserPgRepo::new(pool.get_ref());
 
-    let users = user_repo.get_all().await;
+  let users = user_repo.get_all().await;
 
-    match users {
-        Ok(users) => HttpResponse::Ok().json(users),
-        Err(_err) => HttpResponse::InternalServerError().finish(),
-    }
+  match users {
+    Ok(users) => HttpResponse::Ok().json(users),
+    Err(_err) => HttpResponse::InternalServerError().finish(),
+  }
 }
 
 async fn get_user(pool: web::Data<PgPool>, user_id: web::Path<String>) -> impl Responder {
-    let user_id = Uuid::from_str(&user_id.into_inner());
-    if let Err(_) = user_id {
-        return HttpResponse::BadRequest()
-            .body("Invalid user_id format. Should be a UUID v4 string.");
-    }
-    let user_id = user_id.unwrap();
+  let user_id = Uuid::from_str(&user_id.into_inner());
+  if let Err(_) = user_id {
+    return HttpResponse::BadRequest()
+        .body("Invalid user_id format. Should be a UUID v4 string.");
+  }
+  let user_id = user_id.unwrap();
 
-    let user_repo = UserPgRepo::new(pool.get_ref());
+  let user_repo = UserPgRepo::new(pool.get_ref());
 
-    let user = user_repo.get_by_id(user_id).await;
+  let user = user_repo.get_by_id(user_id).await;
 
-    match user {
-        Ok(user) => HttpResponse::Ok().json(user),
-        Err(err) => match err {
-            RepositoryError::Action(_a) => HttpResponse::NotFound().finish(),
-            RepositoryError::Client(_c) => HttpResponse::InternalServerError().finish(),
-        },
-    }
+  match user {
+    Ok(user) => HttpResponse::Ok().json(user),
+    Err(err) => match err {
+      RepositoryError::Action(_a) => HttpResponse::NotFound().finish(),
+      RepositoryError::Client(_c) => HttpResponse::InternalServerError().finish(),
+    },
+  }
 }
 
 #[derive(Deserialize)]
 struct PostUserReqBody {
-    username: String,
+  username: String,
 }
 
 async fn create_user(body: web::Json<PostUserReqBody>, pool: web::Data<PgPool>) -> impl Responder {
-    let username = &body.username;
-    let user = User::new(username, 0);
+  let username = &body.username;
+  let user = User::new(username, 0);
 
-    let result = UserPgRepo::new(pool.get_ref()).create(user).await;
+  let result = UserPgRepo::new(pool.get_ref()).create(user).await;
 
-    match result {
-        Ok(user) => HttpResponse::Created().json(user),
-        Err(err) => match err {
-            RepositoryError::Action(_) => {
-                HttpResponse::Conflict().body("User with that username already exists")
-            }
-            RepositoryError::Client(_) => HttpResponse::InternalServerError().finish(),
-        },
-    }
+  match result {
+    Ok(user) => HttpResponse::Created().json(user),
+    Err(err) => match err {
+      RepositoryError::Action(_) => {
+        HttpResponse::Conflict().body("User with that username already exists")
+      }
+      RepositoryError::Client(_) => HttpResponse::InternalServerError().finish(),
+    },
+  }
 }
 
-async fn update_user() -> impl Responder {
-    HttpResponse::Ok()
+#[derive(Deserialize)]
+struct PutUserRequestBody {
+  username: String,
+  coins: i64
+}
+
+async fn update_user(pool: web::Data<PgPool>, user_id: web::Path<String>, body: web::Json<PutUserRequestBody>) -> impl Responder {
+  HttpResponse::Ok()
 }
 
 async fn delete_user() -> impl Responder {
-    HttpResponse::Ok()
+  HttpResponse::Ok()
 }
